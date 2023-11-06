95fdc1c8c5da41e8a9fa9ed5a045d7aaeaec1946
Sylvain <sylviemontoute@gmail.com>
2023-11-05 23:40:05 -0400

    third

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ from models.models import *
 
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql+mysqlconnector://root:course123@localhost/STI data'
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URL'], echo=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Berklin21@localhost/sti data'
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
 
 Base.metadata.create_all(engine)
+
+
+# ClientProfile.__table__.create(engine, checkfirst=True)
+# ClientScreening.__table__.create(engine, checkfirst=True)
+# ClientTreatment.__table__.create(engine, checkfirst=True)
+# PartnerManagement.__table__.create(engine, checklist=True)
 
 @app.route('/')
 def index():
     return 'Hello World'
 
-if__name__=='__main__'
-    app.run(debug= True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
